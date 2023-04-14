d546edd5f57104e1634d2fa091902faf5dbb83c9
Ivan Olmo <ivan.olmo@gmail.com>
2023-04-13 23:20:21 -0500

    refactor: add additional test cases

--- src/__tests__/components/JobCard.test.tsx
+++ src/__tests__/components/JobCard.test.tsx
@@ -5,7 +5,7 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import JobCard from "~/components/JobCard";
 import type { RouterOutputs } from "~/utils/api";
 
-type Job = RouterOutputs["job"]["getJobs"][number];
+type Job = RouterOutputs["job"]["getJobs"]["jobs"][number];
 dayjs.extend(relativeTime);
 
 // Example Job data for testing
@@ -17,13 +17,29 @@ const mockJob: Job = {
   logoUrl: "https://test-company.com/logo.svg",
   logoBgColor: "blue",
   jobTitle: "Software Engineer",
-  jobType: "Full-time",
+  jobType: "Full Time",
   location: "New York, NY",
   description: "Lorem ipsum dolor sit amet...",
   salary: "100000",
   requirementId: "1",
   dutyId: "1",
   createdAt: new Date("2023-04-01T00:00:00Z"),
+};
+
+const getRelativeTime = (createdAt: Date) => {
+  return dayjs(createdAt).fromNow();
+};
+
+const expectedTimeFromNow = mockJob.createdAt
+  ? getRelativeTime(mockJob.createdAt)
+  : "";
+
+const decodedURLMatches = (
+  received: string | null,
+  expected: string
+): boolean => {
+  const decodedURL = decodeURIComponent(received || "");
+  return decodedURL.includes(expected);
 };
 
 describe("<JobCard />", () => {
@@ -33,5 +49,31 @@ describe("<JobCard />", () => {
     expect(screen.getByText("Software Engineer")).toBeInTheDocument();
     expect(screen.getByText("Test Company")).toBeInTheDocument();
     expect(screen.getByText("New York, NY")).toBeInTheDocument();
+    expect(screen.getByText("Full Time")).toBeInTheDocument();
+    expect(screen.getByText(expectedTimeFromNow)).toBeInTheDocument();
+    expect(screen.getByAltText("Test Company's logo")).toBeInTheDocument();
+    expect(
+      screen.getByRole("link").querySelector(".absolute.-top-7")
+    ).toHaveStyle({
+      backgroundColor: "blue",
+    });
   });
+});
+
+it("navigates to the correct job page when clicked", () => {
+  render(<JobCard {...mockJob} />);
+  const linkElement = screen.getByRole("link");
+  expect(linkElement.getAttribute("href")).toBe(`/job/${mockJob.id}`);
+});
+
+it("renders the default logo when logoUrl is null", () => {
+  const jobWithNullLogo = { ...mockJob, logoUrl: null };
+  render(<JobCard {...jobWithNullLogo} />);
+  const imageElement = screen.getByAltText("Test Company's logo");
+  expect(
+    decodedURLMatches(
+      imageElement.getAttribute("src"),
+      "https://placehold.co/100"
+    )
+  ).toBeTruthy();
 });
